f064aa6cec74ceb6726be6941c4ab81bfa28190b
Bartek Antos <bartekantos@protonmail.com>
2021-11-17 00:15:04 +0100

    first commit

---
+++ Zadanie/Controllers/FormController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zadanie.Models;
+
+namespace Zadanie.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.ilosc_powtorzen >= 1 && model.ilosc_powtorzen <= 5)
+                {
+                    model.belongs = "Należy";
+                    return View("Answer", model);
+                }
+                else
+                {
+                    model.belongs = "Nie należy";
+                    return View("Answer", model);
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+    }
+}

7f657e476277ecc2a985ffd5e7012ed315e82c25
Bartek Antos <bartekantos@protonmail.com>
2021-11-17 18:15:15 +0100

    poprawione

--- Zadanie/Controllers/FormController.cs
+++ Zadanie/Controllers/FormController.cs
@@ -4,30 +4,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Zadanie.Models;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace Zadanie.Controllers
 {
     public class FormController : Controller
     {
+        private readonly ILogger<FormController> _logger;
+
+        public FormController(ILogger<FormController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+
         [HttpPost]
-        public IActionResult Index(FormModel model)
+        public IActionResult Answer (FormModel model)
         {
             if (ModelState.IsValid)
             {
                 if (model.ilosc_powtorzen >= 1 && model.ilosc_powtorzen <= 5)
                 {
                     model.belongs = "Należy";
-                    return View("Answer", model);
+                    return View( model);
                 }
                 else
                 {
                     model.belongs = "Nie należy";
-                    return View("Answer", model);
+                    return View( model);
                 }
             }
             else
--- Zadanie/Startup.cs
+++ Zadanie/Startup.cs
@@ -35,7 +35,7 @@ namespace Zadanie
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Form/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -50,7 +50,7 @@ namespace Zadanie
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Form}/{action=Index}/{id?}");
             });
         }
     }
